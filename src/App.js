ae137e2bd86f4d94f6678283c5858c00035d19db
Personal - Amir Astaneh <amastaneh@gmail.com>
2024-10-03 08:51:15 -0400

    v1.0.5

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-  return (
-    <div className="container">
-      <h1 className="text-center mt-5">Hesab System</h1>
-    </div>
-  );
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-12 col-lg-6 offset-lg-3">
+					<h1 className="text-center mt-5">Hesab System</h1>
+					<p className="text-center mt-5">
+						A next-generation AI-powered accounting and inventory management system
+						featuring advanced voice interaction capabilities.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 
