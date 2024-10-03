3af2b9c92ced0ec8b6f2796eed0e4181e672b2b5
Personal - Amir Astaneh <amastaneh@gmail.com>
2024-10-03 09:37:26 -0400

    v1.0.7

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/home';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('hesabsystem-root'));
 root.render(
 	<React.StrictMode>
-		<App />
+		<Home />
 	</React.StrictMode>
 );
---
+++ src/pages/home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Home = () => <>
+	<div className="container">
+		<div className="row">
+			<div className="col-12 col-lg-6 offset-lg-3">
+				<h1 className="text-center mt-5">Hesab System</h1>
+				<p className="text-center mt-5">
+					A next-generation AI-powered accounting and inventory management system
+					featuring advanced voice interaction capabilities.
+				</p>
+			</div>
+		</div>
+	</div>
+</>
+
+
+export default Home
